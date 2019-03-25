3aa59a47b0166d88ec25e5b3d90af55a2c221b28
Catalin Belcianu <catalin.belcianu@gmail.com>
2019-03-25 13:19:49 +0200

    made the computer move a little smarter

--- lab4/tictactoe/ticscript.js
+++ lab4/tictactoe/ticscript.js
@@ -19,12 +19,70 @@ function foo(clickedDiv){
 
 function think(){
     var arr = document.getElementsByName("box");
-    for(i=0; i<arr.length; i++){
-        var htmlstring = arr[i].innerHTML;
-        htmlstring = (htmlstring.trim) ? htmlstring.trim() : htmlstring.replace(/^\s+|\s+$/, '');
-        if(htmlstring == ''){
-            arr[i].innerHTML = 'O';
-            break;
+    htmlstring0 = (arr[0].innerHTML.trim) ? arr[0].innerHTML.trim() : arr[0].innerHTML.replace(/^\s+|\s+$/, '');
+    htmlstring1 = (arr[1].innerHTML.trim) ? arr[1].innerHTML.trim() : arr[1].innerHTML.replace(/^\s+|\s+$/, '');
+    htmlstring2 = (arr[2].innerHTML.trim) ? arr[2].innerHTML.trim() : arr[2].innerHTML.replace(/^\s+|\s+$/, '');
+    htmlstring3 = (arr[3].innerHTML.trim) ? arr[3].innerHTML.trim() : arr[3].innerHTML.replace(/^\s+|\s+$/, '');
+    htmlstring4 = (arr[4].innerHTML.trim) ? arr[4].innerHTML.trim() : arr[4].innerHTML.replace(/^\s+|\s+$/, '');
+    htmlstring5 = (arr[5].innerHTML.trim) ? arr[5].innerHTML.trim() : arr[5].innerHTML.replace(/^\s+|\s+$/, '');
+    htmlstring6 = (arr[6].innerHTML.trim) ? arr[6].innerHTML.trim() : arr[6].innerHTML.replace(/^\s+|\s+$/, '');
+    htmlstring7 = (arr[7].innerHTML.trim) ? arr[7].innerHTML.trim() : arr[7].innerHTML.replace(/^\s+|\s+$/, '');
+    htmlstring8 = (arr[8].innerHTML.trim) ? arr[8].innerHTML.trim() : arr[8].innerHTML.replace(/^\s+|\s+$/, '');
+
+    if(htmlstring0=='X' && htmlstring2=='X' && htmlstring1=='')
+        arr[1].innerHTML = 'O';
+    else if(htmlstring0=='X' && htmlstring6=='X' && htmlstring3=='')
+        arr[3].innerHTML = 'O';
+    else if(htmlstring0=='X' && htmlstring8=='X' && htmlstring4=='')
+        arr[4].innerHTML = 'O';
+    else if(htmlstring0=='X' && htmlstring1=='X' && htmlstring2=='')
+        arr[2].innerHTML = 'O';
+    else if(htmlstring0=='X' && htmlstring3=='X' && htmlstring6=='')
+        arr[6].innerHTML = 'O';
+    else if(htmlstring0=='X' && htmlstring4=='X' && htmlstring8=='')
+        arr[8].innerHTML = 'O';
+    else if(htmlstring2=='X' && htmlstring1=='X' && htmlstring0=='')
+        arr[0].innerHTML = 'O';
+    else if(htmlstring2=='X' && htmlstring4=='X' && htmlstring6=='')
+        arr[6].innerHTML = 'O';
+    else if(htmlstring2=='X' && htmlstring5=='X' && htmlstring8=='')
+        arr[8].innerHTML = 'O';
+    else if(htmlstring6=='X' && htmlstring3=='X' && htmlstring0=='')
+        arr[0].innerHTML = 'O';
+    else if(htmlstring6=='X' && htmlstring4=='X' && htmlstring2=='')
+        arr[2].innerHTML = 'O';
+    else if(htmlstring6=='X' && htmlstring7=='X' && htmlstring8=='')
+        arr[8].innerHTML = 'O';
+    else if(htmlstring8=='X' && htmlstring4=='X' && htmlstring0=='')
+        arr[0].innerHTML = 'O';
+    else if(htmlstring8=='X' && htmlstring7=='X' && htmlstring6=='')
+        arr[6].innerHTML = 'O';
+    else if(htmlstring8=='X' && htmlstring5=='X' && htmlstring2=='')
+        arr[2].innerHTML = 'O';
+    else if(htmlstring8=='X' && htmlstring2=='X' && htmlstring5=='')
+        arr[5].innerHTML = 'O';
+    else if(htmlstring8=='X' && htmlstring6=='X' && htmlstring7=='')
+        arr[7].innerHTML = 'O';
+    else if(htmlstring3=='X' && htmlstring5=='X' && htmlstring4=='')
+        arr[4].innerHTML = 'O';
+    else if(htmlstring1=='X' && htmlstring7=='X' && htmlstring4=='')
+        arr[4].innerHTML = 'O';
+    else if(htmlstring3=='X' && htmlstring4=='X' && htmlstring5=='')
+        arr[5].innerHTML = 'O';
+    else if(htmlstring1=='X' && htmlstring4=='X' && htmlstring7=='')
+        arr[7].innerHTML = 'O';
+    else if(htmlstring5=='X' && htmlstring4=='X' && htmlstring3=='')
+        arr[3].innerHTML = 'O';
+    else if(htmlstring7=='X' && htmlstring4=='X' && htmlstring1=='')
+        arr[1].innerHTML = 'O';
+    else{
+        for(i=0; i<arr.length; i++){
+            var htmlstring = arr[i].innerHTML;
+            htmlstring = (htmlstring.trim) ? htmlstring.trim() : htmlstring.replace(/^\s+|\s+$/, '');
+            if(htmlstring == ''){
+                arr[i].innerHTML = 'O';
+                break;
+            }
         }
     }
 }
