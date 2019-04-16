b1a8ed0318462b2c7439023e20f0ede36023ad10
Catalin Belcianu <catalin.belcianu@gmail.com>
2019-04-16 17:04:40 +0300

    refactored the code

--- lab5/dialog.js
+++ lab5/dialog.js
@@ -1,54 +1,47 @@
-function open() {
-    $('#dialog').show();
-}
-
-function close() {
-    $('#dialog').hide();
-}
-
 $(document).ready(function () {
-    close();
+    $('#dialog').hide();
     $('#closebtn').click(function () {
-        close();
+        $('#dialog').hide();
     });
     $('#opener').click(function () {
-        open();
+        $('#dialog').show();
     });
     
 });
 
-var mydragg = function(){
-    return {
-        move : function(divid,xpos,ypos){
-            divid.style.left = xpos + 'px';
-            divid.style.top = ypos + 'px';
-        },
-        startMoving : function(divid,container,evt){
+$(document).ready(function () {
+    var divid = $('#dialog').get(0);
+    var move = function(divid,xpos, ypos){
+        divid.style.left = xpos + 'px';
+        divid.style.top = ypos + 'px';
+    };
+    $('#dialog').mousedown( () => {
+        $(document).unbind('mousemove');
+        evt = window.event;
+        var posX = evt.clientX, posY = evt.clientY, divTop = divid.style.top, divLeft = divid.style.left,
+            eWi = parseInt(divid.style.width), eHe = parseInt(divid.style.height),
+            cWi = parseInt($('#dialog').get(0).style.width), cHe = parseInt($('#dialog').get(0).style.height);
+        $('#dialog').get(0).style.cursor='move';
+        divTop = divTop.replace('px','');
+        divLeft = divLeft.replace('px','');
+        var diffX = posX - divLeft, diffY = posY - divTop;
+        $(document).mousemove( (evt) =>{
             evt = evt || window.event;
-            var posX = evt.clientX, posY = evt.clientY, divTop = divid.style.top, divLeft = divid.style.left,
-                eWi = parseInt(divid.style.width), eHe = parseInt(divid.style.height), cWi = parseInt(document.getElementById(container).style.width),
-                cHe = parseInt(document.getElementById(container).style.height);
-            document.getElementById(container).style.cursor='move';
-            divTop = divTop.replace('px','');
-            divLeft = divLeft.replace('px','');
-            var diffX = posX - divLeft, diffY = posY - divTop;
-            document.onmousemove = function(evt){
-                evt = evt || window.event;
-                var posX = evt.clientX,
-                    posY = evt.clientY,
-                    aX = posX - diffX,
-                    aY = posY - diffY;
-                    if (aX < 0) aX = 0;
-                    if (aY < 0) aY = 0;
-                    if (aX + eWi > cWi) aX = cWi - eWi;
-                    if (aY + eHe > cHe) aY = cHe -eHe;
-                mydragg.move(divid,aX,aY);
-            }
-        },
-        stopMoving : function(container){
-            var a = document.createElement('script');
-            document.getElementById(container).style.cursor='default';
-            document.onmousemove = function(){}
-        },
-    }
-}();
+            var posX = evt.clientX,
+                posY = evt.clientY,
+                aX = posX - diffX,
+                aY = posY - diffY;
+                if (aX < 0) aX = 0;
+                if (aY < 0) aY = 0;
+                if (aX + eWi > cWi) aX = cWi - eWi;
+                if (aY + eHe > cHe) aY = cHe -eHe;
+            move(divid,aX,aY);
+        });
+    });
+    $('#dialog').mouseup( () => {
+        var a = document.createElement('script');
+        $(document).unbind('mousemove');
+        $('#dialog').get(0).style.cursor='default';
+        $(document).mousemove( () => {});
+    });
+});
