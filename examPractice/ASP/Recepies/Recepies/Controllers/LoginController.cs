63f4a555cb7b2aa31ca49ef5f0f1776924cf4ea2
Catalin Belcianu <catalin.belcianu@gmail.com>
2019-06-13 08:21:24 +0300

    asp

---
+++ examPractice/ASP/Recepies/Recepies/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Recepies.DataAbstractionLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Recepies.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string ValidateData()
+        {
+            string username = Request.Params["username"];
+            string password = Request.Params["password"];
+            DAL dal = new DAL();
+            String result = dal.ValidateData(username, password);
+
+            return result;
+
+        }
+    }
+}
---
+++ examPractice/ASP/Recepies/Recepies/DataAbstractionLayer/DAL.cs
@@ -0,0 +1,54 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Recepies.DataAbstractionLayer
+{
+    public class DAL
+    {
+        public string ValidateData(string user, string pass)
+        {
+            string result = "";
+
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=localhost;uid=root;pwd=;database=aspmvc;";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select * from users where username='" + user + "' and password='" + pass + "'";
+                MySqlDataReader myreader = cmd.ExecuteReader();
+
+                while (myreader.Read())
+                {
+                    result += myreader.GetString("name");
+                }
+
+                if (myreader.HasRows)
+                {
+
+                    result += "success";
+                }
+                else
+                {
+                    result += "error";
+                }
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+            return result;
+        }
+    }
+}
